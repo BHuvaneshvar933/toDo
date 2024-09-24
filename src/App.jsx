6d5c54a54f160fa582d30332a51fe7933f00a657
C Bhuvaneshvar Reddy <bhuvaneshvar.r.c@gmail.com>
2024-09-25 00:31:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -71,7 +71,7 @@ function App() {
         <Heading />
         <div className="absolute max-sm:w-full items-center">
           <Input />
-          <div className="ml-16 " id="sec">
+          <div className="ml-16 max-lg:ml-8" >
             <button
               className={
                 "" +
