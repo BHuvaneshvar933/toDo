323d0e5426b017e67a3e9f98a811bddeaa86844d
C Bhuvaneshvar Reddy <bhuvaneshvar.r.c@gmail.com>
2024-10-13 13:22:44 +0530

    Update Heading.jsx

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -2,13 +2,19 @@ import React from "react";
 
 const Heading = () => {
   return (
-    <h1 className="text-center text-4xl mt-5 max-lg:text-center max-sm:ml-28 max-lg:ml-52 ">
+    <h1 className="text-center text-4xl mt-5 
+                   max-lg:text-3xl 
+                   max-sm:text-2xl 
+                   max-sm:ml-0 
+                   max-lg:ml-0">
       TO{" "}
-      <span className="bg-gradient-to-r from-orange-500 to-red-800 text-transparent  bg-clip-text">
+      <span className="bg-gradient-to-r from-orange-500 to-red-800 text-transparent bg-clip-text">
         DO
       </span>{" "}
       List
     </h1>
   );
 };
+
 export default Heading;
+
